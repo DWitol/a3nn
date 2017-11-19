106a57e9682d69fb71f9d94ee4ad74ef4a9c97ed
Kelly Maclauchlan <kelly maclauchlan>
2017-11-19 15:30:32 -0500

    nn in there but the tensor sizes are not correct for question 3

--- a3q3.py
+++ a3q3.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-
+import numpy as np
 from time import time
 import logging
 import matplotlib.pyplot as plt
@@ -12,6 +12,10 @@ from sklearn.metrics import confusion_matrix
 from sklearn.decomposition import PCA
 from sklearn.svm import SVC
 import ssl
+import tensorflow as tf
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+from tensorflow.examples.tutorials.mnist import input_data
 
 # This restores the same behavior as before.
 # need python imageing library to run PIL to run 
@@ -80,33 +84,99 @@ print(classification_report(y_test, y_pred, target_names=target_names))
 print(confusion_matrix(y_test, y_pred, labels=range(n_classes)))
 
 
-def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
-    """Helper function to plot a gallery of portraits"""
-    plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
-    plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.90, hspace=.35)
-    for i in range(n_row * n_col):
-        plt.subplot(n_row, n_col, i + 1)
-        plt.imshow(images[i].reshape((h, w)), cmap=plt.cm.gray)
-        plt.title(titles[i], size=12)
-        plt.xticks(())
-        plt.yticks(())
+# def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
+#     """Helper function to plot a gallery of portraits"""
+#     plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
+#     plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.90, hspace=.35)
+#     for i in range(n_row * n_col):
+#         plt.subplot(n_row, n_col, i + 1)
+#         plt.imshow(images[i].reshape((h, w)), cmap=plt.cm.gray)
+#         plt.title(titles[i], size=12)
+#         plt.xticks(())
+#         plt.yticks(())
 
 
-# plot the result of the prediction on a portion of the test set
+# # plot the result of the prediction on a portion of the test set
 
-def title(y_pred, y_test, target_names, i):
-    pred_name = target_names[y_pred[i]].rsplit(' ', 1)[-1]
-    true_name = target_names[y_test[i]].rsplit(' ', 1)[-1]
-    return 'predicted: %s\ntrue:      %s' % (pred_name, true_name)
+# def title(y_pred, y_test, target_names, i):
+#     pred_name = target_names[y_pred[i]].rsplit(' ', 1)[-1]
+#     true_name = target_names[y_test[i]].rsplit(' ', 1)[-1]
+#     return 'predicted: %s\ntrue:      %s' % (pred_name, true_name)
 
-prediction_titles = [title(y_pred, y_test, target_names, i)
-                     for i in range(y_pred.shape[0])]
+# prediction_titles = [title(y_pred, y_test, target_names, i)
+#                      for i in range(y_pred.shape[0])]
 
-plot_gallery(X_test, prediction_titles, h, w)
+# plot_gallery(X_test, prediction_titles, h, w)
 
-# plot the gallery of the most significative eigenfaces
+# # plot the gallery of the most significative eigenfaces
 
-eigenface_titles = ["eigenface %d" % i for i in range(eigenfaces.shape[0])]
-plot_gallery(eigenfaces, eigenface_titles, h, w)
+# eigenface_titles = ["eigenface %d" % i for i in range(eigenfaces.shape[0])]
+# plot_gallery(eigenfaces, eigenface_titles, h, w)
 
-plt.show()
+# plt.show()
+
+#feed forward nn from class
+
+
+
+inputsORth=X_train_pca
+inputsOrig=X_train
+trainYs=y_train
+testsOrig=X_test
+testsORth=X_test_pca
+testYs=y_train
+sizeX=1850 #784  150 for orthogonal
+sizeY=7 #10
+
+
+
+
+
+
+#with orthoganal
+#trX, trY, teX, teY = inputsORth, trainYs, testsORth, testYs
+trX, trY, teX, teY = inputsOrig, trainYs, testsOrig, testYs
+def init_weights(shape):
+    return tf.Variable(tf.random_normal(shape, stddev=0.01))
+
+
+def model(X, w_h1, w_o):
+    h = tf.nn.sigmoid(tf.matmul(X, w_h1)) # this is a basic mlp
+    return tf.matmul(h, w_o) # note that we dont take the softmax at the end because our cost fn does that for us
+
+#mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+#trX, trY, teX, teY = mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
+
+size_h1 = tf.constant(625, dtype=tf.int32)
+
+X = tf.placeholder("float", [None, sizeX])
+Y = tf.placeholder("float", [None, 7])
+
+w_h1 = init_weights([sizeX, size_h1]) # create symbolic variables
+w_o = init_weights([size_h1, 7])
+
+py_x = model(X, w_h1, w_o)
+
+#resize 
+# temp = trY.shape
+# trY = trY.reshape(temp[0], 1)
+# trY = np.concatenate((1-trY, trY), axis=1)
+# temp = teY.shape
+# teY = teY.reshape(temp[0], 1)
+# teY = np.concatenate((1-teY, teY), axis=1)
+
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=py_x, labels=Y)) # compute costs
+train_op = tf.train.GradientDescentOptimizer(0.05).minimize(cost) # construct an optimizer
+predict_op = tf.argmax(py_x, 1)
+#trX=np.reshape(this_x,(1, 64))
+# Launch the graph in a session
+with tf.Session() as sess:
+    # you need to initialize all variables
+    tf.global_variables_initializer().run()
+    print(range(0,len(trX),128))
+    for i in range(45):
+        for start, end in zip(range(0, len(trX), 128), range(128, len(trX)+1, 128)):
+            sess.run(train_op, feed_dict={X: trX[start:end], Y: trY[start:end]})
+        print(i, np.mean(np.argmax(teY, axis=1) ==
+                         sess.run(predict_op, feed_dict={X: teX})))
